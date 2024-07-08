8c8dc102915d672e9f3230598f96102a9aba56fb
lgyeee <114336688+lgyeee@users.noreply.github.com>
2024-07-08 14:44:30 +0800

    Create StartScreen.cpp

---
+++ src/StartScreen.cpp
@@ -0,0 +1,27 @@
+#include "../include/StartScreen.h"
+
+StartScreen::StartScreen(float width, float height) {
+    if (!font.loadFromFile("assets/fonts/font2.ttf")) {
+        // Handle error
+        printf("test\n");
+    }
+
+    title.setFont(font);
+    title.setString("Snake");
+    title.setCharacterSize(70);
+    title.setFillColor(sf::Color::White);
+    title.setStyle(sf::Text::Bold);
+    title.setPosition(sf::Vector2f(width / 2 - title.getGlobalBounds().width / 2, height / 4));
+
+    instructions.setFont(font);
+    instructions.setString("Press Enter to Start");
+    instructions.setCharacterSize(50);
+    instructions.setFillColor(sf::Color::White);
+    instructions.setStyle(sf::Text::Italic);
+    instructions.setPosition(sf::Vector2f(width / 2 - instructions.getGlobalBounds().width / 2, height / 2));
+}
+
+void StartScreen::draw(sf::RenderWindow& window) {
+    window.draw(title);
+    window.draw(instructions);
+}
